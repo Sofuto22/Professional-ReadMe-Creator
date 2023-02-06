56a87777c9ab63f6618b46ea09eb2e115240e3d6
Sofuto22 <zenigata19@gmail.com>
2023-02-06 18:58:45 -0500

    finished up script

--- Develop/index.js
+++ Develop/index.js
@@ -146,10 +146,30 @@ const questions = () => {
 };
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+const writeToFile = (data) => {
+  return new Promise((resolve,reject) => {
+    fs.writeFile(".generatedREADME.md", data, (err) =>{
+    if (err)
+    {
+      reject(err);
+      return;
+    }
+    resolve({
+      ok: true,
+      message: "File created!",
+    });
+  });
+});
+};
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  questions()
+  .then((response) => generateMarkdown(response))
+  .then((res) =>{
+  writeToFile(res);
+  console.log("Success, check the generated README");
+});
+}
 
 // Function call to initialize app
 init();
